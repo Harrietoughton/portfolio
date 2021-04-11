70b4a3df432304f24d090e1d94739290a0babe95
Harriet Oughton <73295547+Harrietoughton@users.noreply.github.com>
2021-04-11 09:50:20 +0100

    Add files via upload

---
+++ fizz_buzz.rb
@@ -0,0 +1,21 @@
+def fizzbuzz?(index)
+  return true if ((index + 1) % 3).zero? && ((index + 1) % 5).zero?
+end
+
+def fizz_buzz(number)
+  # TODO: return an array of integers, 'Fizz', 'Buzz' or 'FizzBuzz'
+  raise ArgumentError if number < 1
+
+  array = (1..number).to_a
+  array.each_with_index do |_item, index|
+    if fizzbuzz?(index) == true
+      array[index] = "FizzBuzz"
+    elsif ((index + 1) % 3).zero?
+      array[index] = "Fizz"
+    elsif ((index + 1) % 5).zero?
+      array[index] = "Buzz"
+    end
+  end
+end
+
+p fizz_buzz(27)
